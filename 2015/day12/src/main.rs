6b1514385efc3197da29ad0a61e9293f75f5476b
Michael Pyle <michael@Funny-It-Worked-Last-Time.local>
2021-12-02 19:15:32 -0700

    2015 Day 12, Part 2

--- 2015/day12/src/main.rs
+++ 2015/day12/src/main.rs
@@ -1,4 +1,4 @@
-use regex::Regex;
+use serde_json::{ Map, Value };
 
 fn main() {
     use std::time::Instant;
@@ -9,16 +9,40 @@ fn main() {
     let sum = part_one(&input);
     let t2 = Instant::now();
     println!("Part 1: {} ({:?})", sum, t2 - t1);
+
+    let t1 = Instant::now();
+    let sum = part_two(&input);
+    let t2 = Instant::now();
+    println!("Part 2: {} ({:?})", sum, t2 - t1);
 }
 
 fn part_one(s: &str) -> i32 {
+    use regex::Regex;
+
     let re = Regex::new(r"(?-u:\-?\d+)").unwrap();
     re.find_iter(s)
         .map(|v| v.as_str().parse::<i32>().unwrap())
         .sum()
 }
 
+fn part_two(s: &str) -> i64 {
+    let json: Vec<Value> = serde_json::from_str(s).unwrap();
+    json.iter().map(process).sum()
+}
 
+fn process(v: &Value) -> i64 {
+    match v {
+        Value::Number(n)   => n.as_i64().unwrap(),
+        Value::Array(arr)  => arr.iter().map(process).sum(),
+        Value::Object(obj) => process_attributes(obj),
+        _ => 0,
+    }
+}
+
+fn process_attributes(obj: &Map<String, Value>) -> i64 {
+    let red = obj.values().any(|v| v.as_str() == Some("red"));
+    if red { 0 } else { obj.values().map(process).sum() }
+}
 
 #[cfg(test)]
 mod tests {
@@ -30,5 +54,8 @@ mod tests {
 
     let sum = part_one(&input);
     assert_eq!(sum, 191164);
+
+    let sum = part_two(&input);
+    assert_eq!(sum, 87842);
   }
 }
