a5a0ad55c0730f8c06b92595a77676e825cbc980
Michael Pyle <michael@Funny-It-Worked-Last-Time.local>
2021-12-07 16:01:36 -0700

    2017 Day 4

---
+++ 2017/day04/src/main.rs
@@ -0,0 +1,62 @@
+fn main() {
+    use std::fs;
+    use std::time::Instant;
+
+    let input = fs::read_to_string("./input.txt").unwrap();
+    let phrases = load(&input);
+
+    let t1 = Instant::now();
+    let valid = part_one(&phrases);
+    let t2 = Instant::now();
+    println!("Part 1: {} {:?}", valid, t2 - t1);
+
+    let t1 = Instant::now();
+    let valid = part_two(&phrases);
+    let t2 = Instant::now();
+    println!("Part 2: {} {:?}", valid, t2 - t1);
+}
+
+fn load(input: &str) -> Vec<&str> {
+    input.lines().collect()
+}
+
+fn part_one(phrases: &[&str]) -> i32 {
+    use std::collections::HashSet;
+
+    phrases.iter().map(|s| {
+        let parts: Vec<_> = s.split(' ').collect();
+        let unique = HashSet::<&&str>::from_iter(parts.iter());
+        unique.len() == parts.len()
+    }).filter(|valid| *valid).count() as i32
+}
+
+fn part_two(phrases: &[&str]) -> i32 {
+    use itertools::Itertools;
+
+    phrases.iter().map(|s| {
+        let parts: Vec<_> = s.split(' ').collect();
+        parts.iter().combinations(2).any(|v| {
+            v[0].len() == v[1].len() &&  // a little optimization
+            (*v[0]).chars().sorted().eq((*v[1]).chars().sorted())
+        })
+    }).filter(|invalid| !*invalid).count() as i32
+}
+
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use std::fs;
+
+  #[test]
+  fn it_works() {
+    let input = fs::read_to_string("./input.txt").unwrap();
+    let phrases = load(&input);
+
+    let valid = part_one(&phrases);
+    assert_eq!(valid, 325);
+
+    let valid = part_two(&phrases);
+    assert_eq!(valid, 119);
+  }
+}
