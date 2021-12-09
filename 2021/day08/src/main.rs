a1c17e980c4deaed0bcef6f19543aa278938f8c4
Michael Pyle <michael@Funny-It-Worked-Last-Time.local>
2021-12-08 20:09:16 -0700

    Use sets and logic is much faster

--- 2021/day08/src/main.rs
+++ 2021/day08/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 
 fn main() {
     use std::fs;
@@ -17,14 +18,14 @@ fn main() {
     println!("Part 2: {} {:?}", total, t2 - t1);
 }
 
-fn load(input: &str) -> Vec<(Vec<String>, Vec<String>)> {
+fn load(input: &str) -> Vec<(Vec<&str>, Vec<&str>)> {
     input.lines().map(|l| {
         let v = l.split(" | ").collect::<Vec<_>>();
         (split(v[0]), split(v[1]))
     }).collect()
 }
 
-fn part_one(signals: &[(Vec<String>, Vec<String>)]) -> i32 {
+fn part_one(signals: &[(Vec<&str>, Vec<&str>)]) -> i32 {
     signals.iter().map(|(_, outputs)| {
         outputs.iter().filter(|v|
             v.len() == 2 || v.len() == 3 || v.len() == 4 || v.len() == 7
@@ -32,11 +33,10 @@ fn part_one(signals: &[(Vec<String>, Vec<String>)]) -> i32 {
     }).sum()
 }
 
-fn part_two(signals: &[(Vec<String>, Vec<String>)]) -> i32 {
+fn part_two(signals: &[(Vec<&str>, Vec<&str>)]) -> i32 {
     use std::collections::HashMap;
-    use itertools::Itertools;
 
-    let display = [
+    let digits = [
         ("cf",      '1'),
         ("acf",     '7'),
         ("bcdf",    '4'),
@@ -49,63 +49,73 @@ fn part_two(signals: &[(Vec<String>, Vec<String>)]) -> i32 {
         ("abcdefg", '8'),
     ];
 
-    // Brute force using what amounts to a rainbow table.
-    // Generate a vector of all the possible wire mappings.
-    // Use that vector to generate the set of resulting display
-    // values for each wire mapping and capture the mapping,
-    // the display results and the reverse mapping.
-    let base = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
-    let keys = base.iter()
-        .permutations(7)
-        .map(|v| base.iter().zip(v.iter())
-            .map(|(b, k)| (*b, **k)).collect::<HashMap<_, _>>()
-        )
-        .collect::<Vec<_>>();
-    let digits = keys.iter()
-        .map(|k| {
-            let d = display.iter().map(|(s, _)| {
-                let mut d = s.chars().map(|c| k.get(&c).unwrap()).collect::<Vec<_>>();
-                d.sort();
-                String::from_iter(d)
-            })
-            .collect::<Vec<_>>();
-            (
-                k, 
-                k.keys().map(|key| (k.get(key).unwrap(), key)).collect::<HashMap<_, _>>(),
-                d
-            )
-        })
-        .collect::<Vec<_>>();
-
-    // For each signal find the set of "encrypted" display
-    // values which contain all the signal values. That gives
-    // us a valid key. Take the inverse of the key and use it
-    // "decrypt" the output values and look them up in the base
-    // display map to get the "numeric" value. Combine those and
-    // parse the result into an i32.
     signals.iter().map(|(wires, outputs)| {
-        let (_, d, _) = digits.iter()
-            .find(|(_, _, v)| wires.iter().all(|w| v.contains(w)))
-            .unwrap();
+        let one   = find_by_length(&wires, 2);
+        let four  = find_by_length(&wires, 4);
+        let seven = find_by_length(&wires, 3);
+        let eight = find_by_length(&wires, 7);
+
+        let abcdf = &four | &seven;
+        let fives = filter_by_length(&wires, 5);
+        let two   = fives.iter()
+            .filter(|v| (*v - &abcdf).len() == 2)
+            .collect::<Vec<_>>();
+        let tmp   = fives.iter()
+            .filter(|v| (*v - &abcdf).len() == 1)
+            .collect::<Vec<_>>();
+        let two   = *two.first().unwrap();
+        let tmp   = *tmp.first().unwrap();
+
+        let bd    = &four - &one;
+        let b     = &bd - two;
+        let d     = &bd - &b;
+        let g     = tmp - &abcdf;
+        let f     = &abcdf - &(two | &bd);
+        let a     = &seven - &one;
+        let nine  = &(&four | &a) | &g;
+        let e     = &eight - &nine;
+        let c     = &one - &f;
+
+        let key = HashMap::from([
+            (as_char(&a), 'a'),
+            (as_char(&b), 'b'),
+            (as_char(&c), 'c'),
+            (as_char(&d), 'd'),
+            (as_char(&e), 'e'),
+            (as_char(&f), 'f'),
+            (as_char(&g), 'g'),
+        ]);
+
         let v = outputs.iter()
             .map(|s| {
-                let mut chars = s.chars().map(|c| **d.get(&c).unwrap()).collect::<Vec<char>>();
+                let mut chars = s.chars().map(|c| *key.get(&c).unwrap()).collect::<Vec<char>>();
                 chars.sort();
                 String::from_iter(chars)
             })
-            .map(|s| display.iter().find(|(k, _)| s == *k).unwrap())
+            .map(|s| digits.iter().find(|(k, _)| s == *k).unwrap())
             .map(|(_, v)| v)
             .collect::<String>();
         v.parse::<i32>().unwrap()
     }).sum()
 }
 
-fn split(line: &str) -> Vec<String> {
-    line.split(' ').map(|s| {
-        let mut chars = s.chars().collect::<Vec<char>>();
-        chars.sort();
-        String::from_iter(chars)
-    }).collect()
+fn split(line: &str) -> Vec<&str> {
+    line.split(' ').collect::<Vec<_>>()
+}
+
+fn find_by_length(wires: &[&str], len: usize) -> HashSet<char> {
+    wires.iter().find(|s| s.len() == len).unwrap().chars().collect()
+}
+
+fn filter_by_length(wires: &[&str], len: usize) -> Vec<HashSet<char>> {
+    wires.iter()
+        .filter(|s| s.len() == len)
+        .map(|s| s.chars().collect::<HashSet<_>>())
+        .collect()
+}
+
+fn as_char(set: &HashSet<char>) -> char {
+    *set.iter().next().unwrap()
 }
 
 
