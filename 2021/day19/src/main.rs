e1cac31ae4ef24ae9dbbc7b82271a49f68f4e5cc
Michael Pyle <michael@Funny-It-Worked-Last-Time.local>
2022-01-06 22:51:31 -0700

    2021 Day 19, Part 1

--- 2021/day19/src/main.rs
+++ 2021/day19/src/main.rs
@@ -1,87 +1,254 @@
+use lazy_static::lazy_static;
+use nalgebra::{Matrix3, Point3};
+
+lazy_static! {
+    static ref ROTATIONS: [Matrix3<i32>; 24] = [
+        Matrix3::new(1, 0, 0, 0, 1, 0, 0, 0, 1),
+        Matrix3::new(1, 0, 0, 0, 0, 1, 0, -1, 0),
+        Matrix3::new(1, 0, 0, 0, -1, 0, 0, 0, -1),
+        Matrix3::new(1, 0, 0, 0, 0, -1, 0, 1, 0),
+        Matrix3::new(0, 1, 0, 0, 0, 1, 1, 0, 0),
+        Matrix3::new(0, 1, 0, 1, 0, 0, 0, 0, -1),
+        Matrix3::new(0, 1, 0, 0, 0, -1, -1, 0, 0),
+        Matrix3::new(0, 1, 0, -1, 0, 0, 0, 0, 1),
+        Matrix3::new(0, 0, 1, 1, 0, 0, 0, 1, 0),
+        Matrix3::new(0, 0, 1, 0, 1, 0, -1, 0, 0),
+        Matrix3::new(0, 0, 1, -1, 0, 0, 0, -1, 0),
+        Matrix3::new(0, 0, 1, 0, -1, 0, 1, 0, 0),
+        Matrix3::new(-1, 0, 0, 0, -1, 0, 0, 0, 1),
+        Matrix3::new(-1, 0, 0, 0, 0, 1, 0, 1, 0),
+        Matrix3::new(-1, 0, 0, 0, 1, 0, 0, 0, -1),
+        Matrix3::new(-1, 0, 0, 0, 0, -1, 0, -1, 0),
+        Matrix3::new(0, -1, 0, 0, 0, -1, 1, 0, 0),
+        Matrix3::new(0, -1, 0, 1, 0, 0, 0, 0, 1),
+        Matrix3::new(0, -1, 0, 0, 0, 1, -1, 0, 0),
+        Matrix3::new(0, -1, 0, -1, 0, 0, 0, 0, -1),
+        Matrix3::new(0, 0, -1, -1, 0, 0, 0, 1, 0),
+        Matrix3::new(0, 0, -1, 0, 1, 0, 1, 0, 0),
+        Matrix3::new(0, 0, -1, 1, 0, 0, 0, -1, 0),
+        Matrix3::new(0, 0, -1, 0, -1, 0, -1, 0, 0),
+    ];
+}
+
 fn main() {
     use std::fs;
     use std::time::Instant;
     
     let input = fs::read_to_string("./input.txt").unwrap();
-    let scanners = load(&input);
+    let reports = load(&input);
 
     let t1 = Instant::now();
-    let beacons = part_one(&scanners);
+    let beacons = part_one(&reports);
     let t2 = Instant::now();
     println!("Part 1: {} {:?}", beacons, t2 - t1);
 }
 
-#[allow(dead_code)]
-#[derive(Debug)]
-struct Beacon {
-    x: i32,
-    y: i32,
-    z: i32,
-}
+type Beacon = Point3<i32>;
 
-#[allow(dead_code)]
 #[derive(Debug)]
-struct Scanner {
-    facing: i32,
-    rotation: i32,
+#[allow(dead_code)]
+struct Report {
+    id: i32,
+    deltas: Vec<(i32, usize, usize)>,
     beacons: Vec<Beacon>,
 }
 
-fn load(input: &str) -> Vec<Scanner> {
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Scanner {
+    id: i32,
+    origin: (i32, i32, i32),
+    deltas: Vec<(i32, usize, usize)>,
+    beacons: Vec<Beacon>,
+}
+
+fn load(input: &str) -> Vec<Report> {
+    use nalgebra::point;
+
+    let mut id = -1;
     input.split("\n\n").map(|s| {
-        Scanner {
-            facing: 0,
-            rotation: 0,
-            beacons: 
-                s.lines().skip(1).map(|b| {
-                    let mut v = b.split(',').map(|n| n.parse().unwrap());
-                    Beacon {
-                        x: v.next().unwrap(),
-                        y: v.next().unwrap(),
-                        z: v.next().unwrap(),
-                    }
-                }).collect(),
-        }
+        id += 1;
+        let beacons = s.lines().skip(1).map(|b| {
+                let mut it = b.split(',');
+                let x = it.next().unwrap().parse().unwrap();
+                let y = it.next().unwrap().parse().unwrap();
+                let z = it.next().unwrap().parse().unwrap();
+                point![x, y, z]
+            }).collect::<Vec<_>>();
+
+        let mut deltas = beacons.iter().enumerate()
+            .map(|(i, p1)| beacons[i+1..].iter().enumerate()
+                .map(|(j, p2)| (delta(p1, p2), i, j+i+1)).collect::<Vec<_>>()
+            )
+            .flatten()
+            .collect::<Vec<_>>();
+        deltas.sort();
+
+        Report { id, deltas, beacons }
     }).collect()
 }
 
-fn part_one(_scanners: &[Scanner]) -> i64 {
-    use nalgebra::{Reflection, Rotation3, Vector3};
+fn part_one(reports: &[Report]) -> i32 {
+    use std::collections::{HashSet, VecDeque};
+    use nalgebra::point;
 
-    let vec = Vector3::new(8.0, 0.0, 7.0);
+    let mut beacons = HashSet::<Beacon>::from_iter(reports[0].beacons.iter().cloned());
 
-    let angle = Vector3::x() * std::f32::consts::FRAC_PI_2;
-    let rot_x = Rotation3::new(angle);
+    let s0 = Scanner {
+        id: reports[0].id,
+        origin: (0, 0, 0),
+        deltas: reports[0].deltas.clone(),
+        beacons: reports[0].beacons.clone(),
+    };
 
-    let pt = rot_x * vec;
-    println!("x: 90 => {:?}", pt);
-    let pt = rot_x * pt;
-    println!("x:180 => {:?}", pt);
-    let pt = rot_x * pt;
-    println!("x:270 => {:?}", pt);
+    let mut scanners = vec![s0];
+    let mut q = VecDeque::from_iter(reports[1..].iter());
+    while let Some(report) = q.pop_front() {
+        if let Some((scanner, corr)) = find_matches(&scanners, report) {
+            // Find the matching rotation
+            for rot in ROTATIONS.iter() {
+                let pts: HashSet<(i32, i32, i32)> = HashSet::from_iter(
+                    corr.iter().map(|c| {
+                        let b1 = &scanner.beacons[c.0.0];
+                        let b2 = &report.beacons[c.0.1];
+                        let b3 = rot * b2;
+                        (b1.x - b3.x, b1.y - b3.y, b1.z - b3.z)
+                    })
+                );
+                if pts.len() == 1 {
+                    let (dx, dy, dz) = scanner.origin;
+                    let pt = *pts.iter().nth(0).unwrap();
+                    let origin = (pt.0 + dx, pt.1 + dy, pt.2 + dz);
+                    let v = report.beacons.iter()
+                        .map(|b| rot * b)
+                        .collect::<Vec<_>>();
+                    let s = Scanner {
+                        id: report.id,
+                        origin: origin,
+                        deltas: report.deltas.clone(),
+                        beacons: v.clone(),
+                    };
+                    scanners.push(s);
+                    v.iter().for_each(|p| {
+                        let pt = point![p.x + origin.0, p.y + origin.1, p.z + origin.2];
+                        beacons.insert(pt);
+                    });
 
-    let angle = Vector3::y() * std::f32::consts::FRAC_PI_2;
-    let rot_y = Rotation3::new(angle);
+                    break
+                }
+            }
+        } else {
+            q.push_back(report)
+        }
+    }
+/*
+    scanners.iter().for_each(|s| println!("{} {:?}", s.id, s.origin));
+    let mut v = beacons.iter().map(|p| (p.x, p.y, p.z)).collect::<Vec<_>>();
+    v.sort();
+    v.iter().for_each(|pt| println!("{:?}", pt));
+*/
+    beacons.len() as i32
+}
 
-    let pt = rot_y * vec;
-    println!("y: 90 => {:?}", pt);
-    let pt = rot_y * pt;
-    println!("y:180 => {:?}", pt);
-    let pt = rot_y * pt;
-    println!("y:270 => {:?}", pt);
+fn delta(b1: &Beacon, b2: &Beacon) -> i32 {
+    (b1.x - b2.x).abs() +
+    (b1.y - b2.y).abs() +
+    (b1.z - b2.z).abs()
+}
 
-    let angle = Vector3::z() * std::f32::consts::FRAC_PI_2;
-    let rot_z = Rotation3::new(angle);
-    
-    let pt = rot_z * vec;
-    println!("z: 90 => {:?}", pt);
-    let pt = rot_z * pt;
-    println!("z:180 => {:?}", pt);
-    let pt = rot_z * pt;
-    println!("z:270 => {:?}", pt);
+fn find_matches<'a>(scanners: &'a [Scanner], report: &Report)
+    -> Option<(&'a Scanner, Vec<((usize, usize), i32)>)>
+{
+    let mut correlations = None;
 
-    let unit = Vector3::new(1.0, 0.0, 0.0);
-    let refl = Reflection::new(unit, 0);
+    let scanner = scanners.iter().find(|s| {
+        let matches = get_matches(&s.deltas, &report.deltas);
+        let v = correlate(&matches, 5);
+        if v.len() > 10 {
+            correlations = Some(v);
+            true
+        } else {
+            false
+        }
+    });
 
-    0
+    if let Some(s) = scanner {
+        Some((s, correlations.unwrap()))
+    } else {
+        None
+    }
+}
+
+fn get_matches(
+    d1: &Vec<(i32, usize, usize)>,
+    d2: &Vec<(i32, usize, usize)>
+) -> Vec<((i32, usize, usize), (i32, usize, usize))>
+{
+    let mut i = 0;
+    let mut matching = d1.iter().fold(vec![], |mut v, n| {
+        let mut t = d2[i];
+        while t.0 < n.0 && i < d2.len()-1 {
+            i += 1;
+            t = d2[i];
+        }
+        if t.0 == n.0 {
+            v.push((*n, t))
+        }
+        v
+    });
+    matching.sort_by_key(|t| t.0.1);
+
+    matching
+}
+
+fn correlate(
+    matches: &Vec<((i32, usize, usize), (i32, usize, usize))>,
+    threshold: i32
+) -> Vec<((usize, usize), i32)>
+{
+    use std::collections::HashMap;
+
+    let mut map = HashMap::new();
+    matches.iter().for_each(|(t1, t2)| {
+        *map.entry((t1.1, t2.1)).or_insert(0) += 1;
+        *map.entry((t1.1, t2.2)).or_insert(0) += 1;
+        *map.entry((t1.2, t2.1)).or_insert(0) += 1;
+        *map.entry((t1.2, t2.2)).or_insert(0) += 1;
+    });
+
+    let mut counts = map.iter()
+        .filter_map(|e| 
+            if *e.1 > threshold {
+                Some((*e.0, *e.1))
+            } else {
+                 None
+        }).collect::<Vec<_>>();
+    counts.sort_by(|a, b| b.1.cmp(&a.1));
+
+    counts
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    #[test]
+    fn it_works() {
+        let input = fs::read_to_string("./input.txt").unwrap();
+        let reports = load(&input);
+
+        let beacons = part_one(&reports);
+        assert_eq!(beacons, 79);
+    }
+
+    #[test]
+    fn sample() {
+        let input = fs::read_to_string("./test.txt").unwrap();
+        let reports = load(&input);
+
+        let beacons = part_one(&reports);
+        assert_eq!(beacons, 79);
+    }
 }
