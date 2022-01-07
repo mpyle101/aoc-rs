231d92d60fc00d782ab2ab9e293598ed4c137161
Michael Pyle <michael@Funny-It-Worked-Last-Time.local>
2022-01-07 13:42:29 -0700

    2021 Day 21, Part 2, DP

--- 2021/day21/src/main.rs
+++ 2021/day21/src/main.rs
@@ -31,7 +31,7 @@ fn part_one(starting: &[i32]) -> i32 {
     let mut r = 1;
 
     while score[p] < 1000 {
-        p = 1 - p;
+        p = 1 - p;  // flips the player back and forth
         pos[p] = (pos[p] + 3 * r + 3) % 10;
         score[p] += pos[p] + 1;
         r += 3;
@@ -41,30 +41,81 @@ fn part_one(starting: &[i32]) -> i32 {
 }
 
 fn part_two(starting: &[i32]) -> u64 {
-    use std::cmp::max;
+    // The number of won states at each roll.
+    let wins1 = wins(starting[0]);
+    let wins2 = wins(starting[1]);
 
-    // roll result to frequency
-    let rf = [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)];
+    // Number of unfinished states at each roll. The total number
+    // of possible states minus the number of won states. The total
+    // number of possible states is the last number of states times
+    // 27 (possible 2 die rolls).
+    let states1 = wins1.iter()
+        .skip(1).fold(vec![27], |mut v, w| {
+            if let Some(&n) = v.last() { v.push(n * 27 - w) }
+            v
+        });
+    let states2 = wins2.iter()
+        .skip(1).fold(vec![27], |mut v, w| {
+            if let Some(&n) = v.last() { v.push(n * 27 - w) }
+            v
+        });
 
-    let pos = [(starting[0] - 1) as u64, (starting[1] - 1) as u64];
-    let won = wins(pos[0], 21, pos[1], 21, &rf);
+    // Since player 1 goes first, they match against the unfinished
+    // states in player 2's previous roll.
+    let tot1 = wins1.iter().skip(1).zip(states2.iter())
+        .map(|(w, s)| w * s)
+        .collect::<Vec<_>>();
 
-    max(won.0, won.1)
+    // Since player 2 goes second, they match against the same roll in
+    // player 1's unfinished states.
+    let tot2 = wins2.iter().zip(states1.iter())
+        .map(|(w, s)| w * s)
+        .collect::<Vec<_>>();
+
+    let sum1 = tot1.iter().sum::<u64>();
+    let sum2 = tot2.iter().sum::<u64>();
+
+    sum1.max(sum2)
 }
 
-fn wins(p1: u64, s1: i64, p2: u64, s2: i64, rf: &[(u64, u64); 7],
-) -> (u64, u64) {
-    if s2 <= 0 {
-        // Because the player flips back and forth
-        (0, 1)
-    } else {
-        rf.iter().fold((0, 0), |w, (r, f)| {
-            let c = wins(p2, s2, (p1+r) % 10, s1 - 1 - ((p1+r) % 10) as i64, rf);
-            // Add the "swapped" values from c to w
-            (w.0 + f * c.1, w.1 + f * c.0)
+fn wins(pos: i32) -> Vec<u64> {
+    use std::collections::HashMap;
+
+    // dice result to frequency of rolls producing that value
+    let df = [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)];
+
+    // Number of unfinished states for a given position/score
+    // combination.
+    let mut states = HashMap::new();
+    states.insert((pos, 0), 1);
+
+    // Number of wins on a given roll
+    let mut wins = Vec::new();
+    while !states.is_empty() {
+        // Count the wins for the next set of rolls.
+        wins.push(0);
+        states = states.iter().fold(HashMap::new(), |mut m, ((p, s), c)| {
+            df.iter().for_each(|(d, f)| {
+                let pos   = (p + d - 1) % 10 + 1;
+                let score = s + pos;
+                if score < 21 {
+                    // Gonna need to roll again, add the new number of possibilities,
+                    // which is all the old ones time the frequency of this dice roll.
+                    *m.entry((pos, score)).or_insert(0) += c * f;
+                } else if let Some(last) = wins.last_mut() {
+                    // Number of wins goes up by the number of possibliities
+                    // for the state times the frequency of this roll result.
+                    *last += c * f;
+                }
+            });
+
+            m
         })
     }
+
+    wins
 }
+
 
 #[cfg(test)]
 mod tests {
