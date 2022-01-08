5e7931d2333c9a803e41f890ceb1045edef24106
Michael Pyle <michael@Funny-It-Worked-Last-Time.local>
2022-01-08 15:42:30 -0700

    2015 Day 20, Part 2 and much, much faster

--- 2015/day20/src/main.rs
+++ 2015/day20/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     use std::time::Instant;
-/*
+
     let t1 = Instant::now();
     let house = part_one(34000000);
     let t2 = Instant::now();
     println!("Part 1: {} ({:?})", house, t2 - t1);
-*/
+
     let t1 = Instant::now();
     let house = part_two(34000000);
     let t2 = Instant::now();
@@ -14,59 +14,52 @@ fn main() {
 
 #[allow(dead_code)]
 fn part_one(presents: i32) -> i32 {
-    let mut elves = vec![];
-
     let mut house = 0;
-    let mut count = 0;
 
-    while count < presents {
+    loop {
         house += 1;
-        elves.push((house * 2, house));
-        count = house * 10;
-        for i in 0..house/2 {
-            let mut elf = elves.get_mut(i as usize).unwrap();
-            if elf.0 == house {
-                elf.0 += elf.1;
-                count += elf.1 * 10;
-            }
+        if elves(house).iter().map(|n| n * 10).sum::<i32>() >= presents {
+            break house
         }
     }
-
-    house
 }
 
 #[allow(dead_code)]
 fn part_two(presents: i32) -> i32 {
     use std::collections::HashMap;
 
-    let mut elves = HashMap::new();
+    let mut active = HashMap::new();
 
     let mut house = 0;
-    let mut count = 0;
-
-    while count < presents {
+    loop {
         house += 1;
-        elves.insert(house, (house, 0));
-        count = factors(house).iter().fold(0, |acc, n| {
-            if let Some(elf) = elves.get_mut(n) {
-                let v = n * 11;
+        active.insert(house, (house, 0));
+        let count = elves(house).iter().fold(0, |acc, n| {
+            if let Some(elf) = active.get_mut(n) {
                 elf.0 += n;
                 elf.1 += 1;
-                if elf.1 == 50 { elves.remove(n); }
-                acc + v
+                if elf.1 == 50 { active.remove(n); }
+                acc + n * 11
             } else {
                 acc
             }
         });
-
-        if house % 10000 == 0 { println!("{} {}", house, elves.len()) }
+        if count >= presents {
+            break house
+        }
     }
-
-    house
 }
 
-fn factors(v: i32) -> Vec<i32> {
-    (1..=v/2).filter_map(|n| if v % n == 0 { Some(n) } else { None }).collect()
+fn elves(val: i32) -> Vec<i32> {
+    let end = ((val as f32).sqrt() + 1.0) as i32;
+    (1..=end).fold(vec![], |mut v, n| {
+        if val % n == 0 {
+            v.push(n);
+            v.push(val / n);
+        }
+
+        v
+    })
 }
 
 #[cfg(test)]
@@ -77,5 +70,8 @@ mod tests {
   fn it_works() {
     let house = part_one(34000000);
     assert_eq!(house, 786240);
+
+    let house = part_two(34000000);
+    assert_eq!(house, 831600);
   }
 }
