af82b4d0c68d5968c912f7215621c4448c43a423
Michael Pyle <michael@Funny-It-Worked-Last-Time.local>
2022-01-10 18:10:35 -0700

    2015 Day 11

---
+++ 2015/day11/src/main.rs
@@ -0,0 +1,96 @@
+
+fn main() {
+    use std::time::Instant;
+
+    let t1 = Instant::now();
+    let password = part_one("hepxcrrq");
+    let t2 = Instant::now();
+    println!("Part 1: {} ({:?})", password, t2 - t1);
+
+    let t1 = Instant::now();
+    let password = part_two(&password);
+    let t2 = Instant::now();
+    println!("Part 2: {} ({:?})", password, t2 - t1);
+}
+
+fn part_one(pword: &str) -> String {
+    // Turn our input into an array of zero based bytes.
+    // This will make incrementing trivial using mod 26.
+    // hepxcrrq
+    let mut password = [0u8;8];
+    pword.as_bytes().iter()
+        .enumerate()
+        .for_each(|(i, b)| password[i] = *b - ('a' as u8));
+
+    increment(&mut password);
+    while !is_valid(&password) {
+        increment(&mut password);
+    }
+
+    // Rehydrate back into 'a' based characters.
+    password.iter().map(|b| (b + 'a' as u8) as char).collect::<String>()
+}
+
+fn part_two(pword: &str) -> String {
+    // Turn our input into an array of zero based bytes.
+    // This will make incrementing trivial using mod 26.
+    // hepxcrrq
+    let mut password = [0u8;8];
+    pword.as_bytes().iter()
+        .enumerate()
+        .for_each(|(i, b)| password[i] = *b - ('a' as u8));
+
+    increment(&mut password);
+    while !is_valid(&password) {
+        increment(&mut password);
+    }
+
+    // Rehydrate back into 'a' based characters.
+    password.iter().map(|b| (b + 'a' as u8) as char).collect::<String>()
+}
+
+fn increment(s: &mut [u8;8]) {
+    let mut i = 7;
+    s[i] = (s[i] + 1) % 26;
+    while s[i] == 0 && i > 0 {
+        i -= 1;
+        s[i] = (s[i] + 1) % 26;
+    }
+}
+
+fn is_valid(s: &[u8;8]) -> bool {
+    let mut pairs = 0;
+
+    let mut straight = false;
+    for i in 0..6 {
+        straight |= s[i+1] == s[i] + 1 && s[i+2] == s[i] + 2
+    }
+    if straight {
+        let mut i = 0;
+        while i < 7 && pairs < 2 {
+            i = if s[i] == s[i+1] {
+                pairs += 1;
+                i + 2
+            } else {
+                i + 1
+            }
+        }
+    }
+
+    pairs > 1
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let password = part_one("hepxcrrq");
+        assert_eq!(password, "hepxxyzz");
+
+        let password = part_one("hepxxyzz");
+        assert_eq!(password, "heqaabcc");
+    }
+}
