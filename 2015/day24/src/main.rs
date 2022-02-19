53aff8e88e6198b833dbece6c525a76ee74457d5
Michael Pyle <michael@Funny-It-Worked-Last-Time.local>
2022-02-18 21:09:19 -0700

    Clean up if/Some/else/None blocks

--- 2015/day24/src/main.rs
+++ 2015/day24/src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let t1 = Instant::now();
     let qe = part_one(&weights);
     let t2 = Instant::now();
-    println!("Part 1: {} ({:?})", qe, t2 - t1);
+    println!("Part 1: {qe} ({:?})", t2 - t1);
 
     let t1 = Instant::now();
     let qe = part_two(&weights);
     let t2 = Instant::now();
-    println!("Part 2: {} ({:?})", qe, t2 - t1);
+    println!("Part 2: {qe} ({:?})", t2 - t1);
 }
 
 fn load(input: &str) -> Vec<i64> {
@@ -27,12 +27,12 @@ fn part_one(weights: &[i64]) -> i64 {
     // Manually worked our way to 6 because 2-5 returned no results.
     // Could put an outer loop to work our way up to handle any data
     // set.
-    weights.iter().combinations(6).filter_map(|v| 
-        if v.iter().cloned().sum::<i64>() == target { Some(v) } else { None }
-    )
-    .map(|v| v.iter().cloned().product::<i64>())
-    .min()
-    .unwrap()
+    weights.iter()
+        .combinations(6)
+        .filter_map(|v| (v.iter().copied().sum::<i64>() == target).then(||v))
+        .map(|v| v.iter().copied().product::<i64>())
+        .min()
+        .unwrap()
 }
 
 
@@ -41,31 +41,31 @@ fn part_two(weights: &[i64]) -> i64 {
 
     let target = weights.iter().sum::<i64>() / 4;
 
-    // Manually worked our way to 6 because 2 & 3 returned no results.
+    // Manually worked our way to 4 because 2 & 3 returned no results.
     // Could put an outer loop to work our way up to handle any data
     // set.
-    weights.iter().combinations(4).filter_map(|v| 
-        if v.iter().cloned().sum::<i64>() == target { Some(v) } else { None }
-    )
-    .map(|v| v.iter().cloned().product::<i64>())
-    .min()
-    .unwrap()
+    weights.iter()
+        .combinations(4)
+        .filter_map(|v| (v.iter().copied().sum::<i64>() == target).then(||v))
+        .map(|v| v.iter().copied().product::<i64>())
+        .min()
+        .unwrap()
 }
 
 
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn it_works() {
-    let weights = load(include_str!("./input.txt"));
+    #[test]
+    fn it_works() {
+        let weights = load(include_str!("./input.txt"));
 
-    let qe = part_one(&weights);
-    assert_eq!(qe, 11846773891);
+        let qe = part_one(&weights);
+        assert_eq!(qe, 11846773891);
 
-    let qe = part_two(&weights);
-    assert_eq!(qe, 80393059);
-  }
+        let qe = part_two(&weights);
+        assert_eq!(qe, 80393059);
+    }
 }
