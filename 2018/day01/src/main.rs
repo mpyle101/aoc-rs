b80bce8d65d98cdbea1a279919474ad20d178cc7
Michael Pyle <michael@Funny-It-Worked-Last-Time.local>
2022-03-02 15:48:30 -0700

    A little cleanup

--- 2018/day01/src/main.rs
+++ 2018/day01/src/main.rs
@@ -1,22 +1,23 @@
 
 fn main() {
-    
     use std::time::Instant;
-    let input = load(include_str!("./input.txt"));
+
+    let input = load(include_str!("./input.txt"))
+        .expect("Loading failed: ");
 
     let t1 = Instant::now();
     let freq = part_one(&input);
     let t2 = Instant::now();
-    println!("Part 1: {} ({:?})", freq, t2 - t1);
+    println!("Part 1: {freq} ({:?})", t2 - t1);
 
     let t1 = Instant::now();
     let freq = part_two(&input);
     let t2 = Instant::now();
-    println!("Part 2: {} ({:?})", freq, t2 - t1);
+    println!("Part 2: {freq} ({:?})", t2 - t1);
 }
 
-fn load(input: &str) -> Vec<i32> {
-    input.lines().map(|v| v.parse::<i32>().unwrap()).collect()
+fn load(input: &str) -> Result<Vec<i32>, std::num::ParseIntError> {
+    input.lines().map(|v| v.parse::<i32>()).into_iter().collect()
 }
 
 fn part_one(deltas: &[i32]) -> i32 {
@@ -29,8 +30,7 @@ fn part_two(deltas: &[i32]) -> i32 {
     let mut value = 0;
     let mut seen = HashSet::new();
     let mut iter = deltas.iter().cycle();
-    while !seen.contains(&value) {
-        seen.insert(value);
+    while seen.insert(value) {
         value += iter.next().unwrap();
     }
 
@@ -44,7 +44,8 @@ mod tests {
 
   #[test]
   fn it_works() {
-    let input = load(include_str!("./input.txt"));
+    let input = load(include_str!("./input.txt"))
+        .expect("Loading failed: ");
 
     let freq = part_one(&input);
     assert_eq!(freq, 445);
