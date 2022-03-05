9a9513570b58223e7fb85a46ffc524a350142cb2
Michael Pyle <michael@Funny-It-Worked-Last-Time.local>
2022-03-04 20:26:37 -0700

    Add timeit function

--- 2021/day01/src/main.rs
+++ 2021/day01/src/main.rs
@@ -3,21 +3,20 @@ use std::error::Error;
 use std::num::ParseIntError;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    use std::time::Instant;
-
     let input = load(&fs::read_to_string("./input.txt")?)?;
 
-    let t1 = Instant::now();
-    let count = part_one(&input);
-    let t2 = Instant::now();
-    println!("Part 1: {} ({:?})", count, t2 - t1);
-
-    let t1 = Instant::now();
-    let count = part_two(&input);
-    let t2 = Instant::now();
-    println!("Part 2: {} ({:?})", count, t2 - t1);
+    timeit("Part 1", || part_one(&input));
+    timeit("Part 2", || part_two(&input));
 
     Ok(())
+}
+
+fn timeit<T>(s: &str, func: impl Fn() -> T)
+    where T: std::fmt::Debug
+{
+    let t = std::time::Instant::now();
+    let result = func();
+    println!("{s}: {:?} ({:?})", result, t.elapsed());
 }
 
 fn load(input: &str) -> Result<Vec<i32>, ParseIntError> {
@@ -27,9 +26,10 @@ fn load(input: &str) -> Result<Vec<i32>, ParseIntError> {
 fn part_one(depths: &[i32]) -> i32 {
     let mut iter = depths.iter();
     let first = iter.next().unwrap();
-    let (_, count) = iter.fold((first, 0), |(last, count), v|
-        (v, count + ((v > last) as i32))
-    );
+    let (_, count) = iter
+        .fold((first, 0), |(last, count), v|
+            (v, count + ((v > last) as i32))
+        );
     
     count
 }
@@ -38,10 +38,11 @@ fn part_two(depths: &[i32]) -> i32 {
     let mut iter = depths.windows(3);
     let w = iter.next().unwrap();
     let first = w.iter().sum::<i32>();
-    let (_, count) = iter.fold((first, 0), |(last, count), v| {
-        let val = v.iter().sum();
-        (val, count + ((val > last) as i32))
-    });
+    let (_, count) = iter
+        .fold((first, 0), |(last, count), v| {
+            let val = v.iter().sum();
+            (val, count + ((val > last) as i32))
+        });
     
     count
 }
